83d21a42c02616d51958f9b8eab87a670d5ede8e
지상은 <wltkddms9909@naver.com>
2022-05-26 22:03:53 +0900

    boj greedy

--- BOJ 알고리즘별 문제풀이/그리디/10610.py
+++ BOJ 알고리즘별 문제풀이/그리디/10610.py
@@ -1,13 +1,13 @@
 n=list(input())
 n.sort(reverse=True)
-sum=0
+ans=0
 
 if '0' not in n:
     print(-1)
 else:
     for i in n:
-        sum+=int(i)
-    if sum%3!=0:
+        ans+=int(i)
+    if ans%3!=0:
         print(-1)
     else:
         print(''.join(n))
