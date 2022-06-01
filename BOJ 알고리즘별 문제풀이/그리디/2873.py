97e80bbae3ec69c8b0ac34610f97f0b9a3024f20
지상은 <wltkddms9909@naver.com>
2022-06-01 16:59:08 +0900

    2873 미해결

--- BOJ 알고리즘별 문제풀이/그리디/2873.py
+++ BOJ 알고리즘별 문제풀이/그리디/2873.py
@@ -1,8 +1,42 @@
 r,c=map(int,input().split())
 
 row=[]
-
+res=""
 for i in range(r):
     buf=list(map(int,input().split()))
     row.append(buf)
 
+if r%2!=0 & c%2==0:
+    for i in range(r//2):
+        res+="R"*(c-1)
+        res+="D"
+        res+="L"*(c-1)
+        res+="D"
+    res+="R"*(c-1)
+elif c%2!=0:
+    for i in range(c//2):
+        res+="D"*(r-1)
+        res+="R"
+        res+="U"*(r-1)
+        res+="R"
+    res+="D"*(r-1)
+
+elif r%2==0 & c%2==0:
+    min=1000
+    pick=[-1,-1]
+    for i in range(r):
+        if c%2==0: # 홀수행일때 (row 가 0부터 시작)
+            for j in range(1,c,2):
+                if row[i][j]<min:
+                    pick=[i,j]
+                    min=row[i][j]
+        else:
+            for j in range(0,c,2):
+                if row[i][j]<min:
+                    pick=[i,j]
+                    min=row[i][j]
+
+
+
+print(res)
+print(pick)
