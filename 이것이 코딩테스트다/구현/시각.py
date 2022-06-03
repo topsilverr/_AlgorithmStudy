b0896289dfa0ba54afb86e772923391665e24be4
지상은 <wltkddms9909@naver.com>
2022-06-03 18:40:47 +0900

    구현 예제풀이

--- 이것이 코딩테스트다/구현/시각.py
+++ 이것이 코딩테스트다/구현/시각.py
@@ -1,2 +1,11 @@
 n=int(input())
 
+res=0
+
+for h in range(n+1):
+    for m in range(60):
+        for s in range(60):
+            if '3' in str(h)+str(m)+str(s): # 숫자를 문자열로 변경하여 3이 있는 시각 개수 세기
+                res+=1
+
+print(res)
