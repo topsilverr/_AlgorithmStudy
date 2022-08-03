081401f21203e854db76ef8d854fd10773e438e1
지상은 <wltkddms9909@naver.com>
2022-08-03 17:30:49 +0900

    브루트포스

---
+++ BOJ 단계별 문제풀이/브루트포스/14888.py
@@ -0,0 +1,19 @@
+from itertools import permutations
+
+
+# n=int(input()) # 숫자 개수
+# a = list(map(int,input().split())) # 숫자 목록
+# s = list(map(int,input().split())) # 연산자 개수 ( +, -, x, / )
+
+# 연산자 배열 만들기 by 순열 (itertools 라이브러리)
+arr=[]
+# for a in range(0,4):
+#     if a == 0:
+#         arr.append('+'*s[a])
+# data = "+-x"
+# result = permutations(data,len(data))
+#
+# for r in result:
+#     print(r)
+
+print(arr.append('+' * 4))
--- BOJ 단계별 문제풀이/브루트포스/2798.py
+++ BOJ 단계별 문제풀이/브루트포스/2798.py
@@ -2,11 +2,18 @@ n,m=map(int,input().split())
 num=list(map(int,input().split()))
 
 num.sort(reverse=True)
+result = 0
 
 for a in num:
     if a >= m:
         del a # 굳이
 
-for a in num:
+for i in range(0,len(num)):
+    for j in range(i+1,len(num)):
+        for k in range(i+2,len(num)):
+            if num[i]+num[j]+num[k] > m :
+                continue
+            else:
+                result=max(result,num[i]+num[j]+num[k])
 
-
+print(result)
