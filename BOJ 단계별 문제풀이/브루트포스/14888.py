e4152a226d3700cd5cae220bfd9048cb54a42728
지상은 <wltkddms9909@naver.com>
2022-08-14 22:38:22 +0900

    14888

--- BOJ 단계별 문제풀이/브루트포스/14888.py
+++ BOJ 단계별 문제풀이/브루트포스/14888.py
@@ -1,19 +1,34 @@
 from itertools import permutations
 
 
-# n=int(input()) # 숫자 개수
-# a = list(map(int,input().split())) # 숫자 목록
-# s = list(map(int,input().split())) # 연산자 개수 ( +, -, x, / )
+n=int(input()) # 숫자 개수
+a = list(map(int,input().split())) # 숫자 목록
+s = list(map(int,input().split())) # 연산자 개수 ( +, -, x, / )
 
 # 연산자 배열 만들기 by 순열 (itertools 라이브러리)
 arr=[]
-# for a in range(0,4):
-#     if a == 0:
-#         arr.append('+'*s[a])
-# data = "+-x"
-# result = permutations(data,len(data))
-#
-# for r in result:
-#     print(r)
 
-print(arr.append('+' * 4))
+for x in range(0,4):
+    if x == 0:
+        for y in range(0,s[x]):
+            arr.append('+')
+    elif x == 1:
+        for y in range(0,s[x]):
+            arr.append('-')
+    elif x == 2:
+        for y in range(0,s[x]):
+            arr.append('*')
+    elif x == 3:
+        for y in range(0,s[x]):
+            arr.append('//')
+
+result = list(permutations(arr,len(arr))) # 집합을 리스트 형식으로 변경 <= permutaions 사용하면 집합으로 반환됨
+res=a[0]
+
+print(result[0][1])
+
+
+
+for r in result:
+    print(r)
+
