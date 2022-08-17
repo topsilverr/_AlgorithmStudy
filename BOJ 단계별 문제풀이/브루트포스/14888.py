539363f850126f968cb3025a13eeaab628b0aa4b
지상은 <wltkddms9909@naver.com>
2022-08-18 00:12:14 +0900

    boj 14888

--- BOJ 단계별 문제풀이/브루트포스/14888.py
+++ BOJ 단계별 문제풀이/브루트포스/14888.py
@@ -1,3 +1,4 @@
+from collections import deque
 from itertools import permutations
 
 
@@ -20,15 +21,41 @@ for x in range(0,4):
             arr.append('*')
     elif x == 3:
         for y in range(0,s[x]):
-            arr.append('//')
+            arr.append('/')
 
 result = list(permutations(arr,len(arr))) # 집합을 리스트 형식으로 변경 <= permutaions 사용하면 집합으로 반환됨
-res=a[0]
+q=deque(result) # 큐를 사용하여 하나씩 꺼내서 계산
 
-print(result[0][1])
+max_result=-1e9
+min_result=1e9
+
+while q:
+    num=q.popleft() # 가능한 연산자 순열 수
+    res=a[0]
+    for i in range(1,len(a)):
+        if num[i-1]=='+':
+            res+=a[i]
+        elif num[i-1]=='-':
+            res-=a[i]
+        elif num[i-1]=='*':
+            res*=a[i]
+        elif num[i-1]=='/':
+            if res < 0 :
+                res=-(abs(res)//a[i])
+            else:
+                res=res//a[i]
+
+    max_result=max(max_result,res)
+    min_result=min(min_result,res)
+
+print(max_result)
+print(min_result)
 
 
 
-for r in result:
-    print(r)
+
+
+
+
+
 
