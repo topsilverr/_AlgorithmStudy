1666cc95d75f3900386e3332874ad122b21a83de
topsilverr <wltkddms9909@naver.com>
2022-10-04 04:32:37 +0900

    dfs 문제 풀이

---
+++ 이것이 코딩테스트다/DFSBFS/음료수얼려먹기.py
@@ -0,0 +1,25 @@
+n,m=map(int,input().split())
+box=[]
+for i in range(n):
+    box.append(list(map(int,input())))
+
+def dfs(x,y):
+    if x<=-1 or x>=n or y <=-1 or y >=m:
+        return False;
+    if box[x][y]==0:
+        box[x][y]=1
+        # 상하좌우 재귀적으로 호출
+        dfs(x-1,y)
+        dfs(x+1,y)
+        dfs(x,y-1)
+        dfs(x,y+1)
+        return True
+    return False
+
+cnt=0
+for i in range(n):
+    for j in range(m):
+        if dfs(i,j)==True:
+            cnt+=1
+
+print(cnt)
