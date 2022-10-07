9f52ef5b97381b86706a4fa2ebff67446afd9f0b
topsilverr <wltkddms9909@naver.com>
2022-10-07 19:49:40 +0900

    #2667 다시 생각하기

---
+++ BOJ 단계별 문제풀이/그래프와 순회_DFSBFS/2667.py
@@ -0,0 +1,35 @@
+n=int(input())
+graph=[]
+
+result=[]
+for i in range(n):
+    graph.append(list(map(int,input())))
+
+def dfs(x, y):
+    if x<=-1 or x>=n or y<=-1 or y>=n:
+        return False
+    elif graph[x][y]==1:
+        graph[x][y]=0
+        global cnt
+        cnt+=1
+        dfs(x+1,y)
+        dfs(x-1,y)
+        dfs(x,y+1)
+        dfs(x,y-1)
+        return True
+    return False
+
+apt=0
+cnt=0
+for i in range(n):
+    for j in range(n):
+        if dfs(i,j):
+            result.append(cnt)
+            apt+=1
+            cnt=0
+
+result.sort()
+print(apt)
+for i in range(len(result)):
+    print(result[i])
+
