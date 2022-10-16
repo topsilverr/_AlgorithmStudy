8e1f147fcc43bf9f294a96103e00888da4815d9f
topsilverr <wltkddms9909@naver.com>
2022-10-16 23:42:54 +0900

    commit

--- BOJ 단계별 문제풀이/그래프와 순회_DFSBFS/1697.py
+++ BOJ 단계별 문제풀이/그래프와 순회_DFSBFS/1697.py
@@ -1,32 +1,28 @@
 from collections import deque
 
-n,k = map(int,input().split())
-time=0
+n, k = map(int, input().split())
+time = 0
 
-dx=[-1,1,n]
+dx = [-1, 1, n]
 
-def bfs(n,k):
-    queue=deque()
+
+def bfs(n, k):
+    queue = deque()
     queue.append(n)
     global time
-    time=0
+    time = 0
     while queue:
-        n=queue.popleft()
+        n = queue.popleft()
         for i in range(3):
-            nx=n+dx[i]
-            if nx <= -1 or nx >100000:
+            nx = n + dx[i]
+            if nx <= -1 or nx > 100000:
                 continue
-            elif nx==k:
+            elif nx == k:
                 return time
             else:
                 queue.append(nx)
-                time+=1
+                time += 1
 
-bfs(n,k)
+
+bfs(n, k)
 print(time)
-
-
-
-
-
-
