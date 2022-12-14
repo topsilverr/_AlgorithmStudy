76e454f68c8d79a54f38ecb3252f92b3dcdfec7d
topsilverr <wltkddms9909@naver.com>
2022-12-14 12:06:26 +0900

    [이코테] 이진탐색 - 부품찾기

--- 이것이 코딩테스트다/이진탐색/부품찾기.py
+++ 이것이 코딩테스트다/이진탐색/부품찾기.py
@@ -1,25 +1,28 @@
 import sys
 
-n=int(input())
-items=list(map(int,input().split()))
+
+def binary_search(list, target, start, end):
+    if start > end:
+        return None
+    mid = (start + end) // 2
+    if list[mid] == target:
+        return list[mid]
+    elif list[mid] < target:
+        return binary_search(list, target, mid + 1, end)
+    elif list[mid] > target:
+        return binary_search(list, target, start, mid - 1)
+
+
+n = int(input())
+items = list(map(int, input().split()))
 items.sort()
 
-m=int(input())
-target=list(map(int,input().split()))
+m = int(input())
+target = list(map(int, input().split()))
 
-def binary_search(list,target,start,end):
-    if start>end:
-        return None
-    mid=(start+end)//2
-    if list[mid]==target:
-        return list[mid]
-    elif list[mid]<target:
-        return binary_search(list,target,mid+1,end)
-    elif list[mid]>target:
-        return binary_search(list,target,start,mid-1)
 for t in target:
-    res=binary_search(items,t,0,n-1)
+    res = binary_search(items, t, 0, n - 1)
     if res != None:
-        print('yes',end=' ')
+        print('yes', end=' ')
     else:
-        print('no',end=' ')
+        print('no', end=' ')
