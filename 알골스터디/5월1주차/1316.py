c4e9f6b1830f77b4d3adef5a11758e5e178b6d89
topsilverr <wltkddms9909@naver.com>
2023-05-01 23:35:29 +0900

    [알골스터디] 5월1주차 1316 sol

---
+++ 알골스터디/5월1주차/1316.py
@@ -0,0 +1,28 @@
+n = int(input())
+
+cnt = 0
+chk = []
+now = ''
+for i in range(n):
+    chk = []
+    word = input()
+    tag = True
+    now = word[0]
+    chk.append(word[0])
+    for w in word[1:]:
+        if w != now and w not in chk:
+            chk.append(w)
+        elif w == now and w in chk and w != chk[-1]:
+            tag = False
+            break
+        elif w != now and w in chk:
+            tag = False
+            break
+        elif w == now and w not in chk:
+            chk.append(w)
+        now = w
+
+    if tag:
+        cnt+=1
+
+print(cnt)
