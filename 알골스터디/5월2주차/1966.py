8e3f888b94c59f47da339f10896aa82723fe67ac
topsilverr <wltkddms9909@naver.com>
2023-05-15 01:31:27 +0900

    [알골스터디] 5월2주차 1966 sol

--- 알골스터디/5월2주차/1966.py
+++ 알골스터디/5월2주차/1966.py
@@ -1,2 +1,22 @@
+from collections import deque
+import sys
 
+t = int(input())
 
+res = []
+
+for _ in range(t):
+    n,m = map(int,input().split())
+    deq = deque(list(map(int,sys.stdin.readline().split())))
+    tar_list = deque(list(range(n)))
+    cnt = 0
+
+    while deq:
+        if max(deq) != deq[0]:
+            deq.append(deq.popleft())
+            tar_list.append(tar_list.popleft())
+        else:
+            cnt += 1
+            deq.popleft()
+            if tar_list.popleft()== m:
+                print(cnt)
