34057e18e0dcecac599fac959baa2a6f3fb8e3ef
topsilverr <wltkddms9909@naver.com>
2023-05-15 00:46:54 +0900

    [알골스터디] 5월2주차 1074 sol

--- 알골스터디/5월2주차/1074.py
+++ 알골스터디/5월2주차/1074.py
@@ -1,27 +1,21 @@
-n,r,c = map(int,input().split())
+n, r, c = map(int, input().split())
 
-res = -1
-reg = 0
-start = 0 # 시작숫자,,,,
-def z(x,y,n):
+res = 0
+
+
+def z(x, y, N):
     global res
-    global reg
-    res+=1
-    if n!= 1:
-        for i in range(x,x+2**n):
-            for j in range(y,y+2**n):
-                z(x,y,n-1)
-                z(x,y+2**(n-1),n-1)
-                z(x+2**(n-1),y,n-1)
-                z(x+2**(n-1),y+2**(n-1),n-1)
-                return
 
-    if n==1 and x <= r <= x+1 and y <= c <= y+1:
-        for i in range(x):
-            for j in range(y):
-                if r == i and c == j:
-                    print(4*(res-1) )
+    if r == x and c == y:
+        print(int(res))
+        exit(0)
+    if not (x <= r < x + N and y <= c < y + N):
+        res += N * N
+        return
 
+    z(x, y, N / 2)
+    z(x, y + N / 2, N / 2)
+    z(x + N / 2, y, N / 2)
+    z(x + N / 2, y + N / 2, N / 2)
 
-
-z(0,0,n)
+z(0, 0, 2 ** n)
